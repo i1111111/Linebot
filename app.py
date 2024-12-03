da1125212c54d5ecc5d456533d78fffb5ddc5d2f
i1111111 <113333204+i1111111@users.noreply.github.com>
2024-12-03 18:55:02 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ import re
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('6EnsTmbA8HylzZ2lBn2SvHBh4nDZsxLwAPFAOpTkkVhs/ZmA3b9c4dfFe+9b6JO/wxX3gqJJgGyPFCUok7JkDtyF1gUgex2lgih6i28nhOb+AOGdNbz9d7dsYQgCR++CydY4ajh2jeEtz1x+sSV1nQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('60Cu38jaKl8XCZL5n8K+TJvs7heExFNDNHKvkYq8fv83wQRdb2G3kJWpVo4B0RzzSv0xlcyvTktzRfK95nK9GiW64PDQtJDYG/zkf+2UjFb6u2U3PrY4+RYbZL+gBOzJ3dwGM94xRCuPx5rDeLB6qAdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('5102e18f461cfcf78a847a33a126ec54')
+handler = WebhookHandler('9f8c5f245a4a4d2734a2e7ac1477f6f0')
 
-line_bot_api.push_message('Uff248f1ad34ea853e4a785d4eb82799e', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('U0d6d8cfdae6abfa3e4628b1ab7b976c9', TextSendMessage(text='你可以開始了'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -42,7 +42,8 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
     if re.match('告訴我秘密',message):
         audio_message = AudioSendMessage(
             original_content_url='https://campus-studio.com/download/twsong.mp3',
